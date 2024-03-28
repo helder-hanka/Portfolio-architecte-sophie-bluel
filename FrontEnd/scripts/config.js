088e05f2b99ff40b6fdcb469250bcaf425e2b05c
helder-hanka <hfernandes238@gmail.com>
2024-03-28 01:36:12 +0100

    Creating worksFetch func

---
+++ FrontEnd/scripts/config.js
@@ -0,0 +1,8 @@
+export const worksFetch = async () => {
+  try {
+    const res = await fetch("http://localhost:5678/api/works");
+    return await res.json();
+  } catch (error) {
+    return console.log(error);
+  }
+};
