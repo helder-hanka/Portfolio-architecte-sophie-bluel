6428df3ed9e73f4e6c47c337bb7565ee799e5965
helder-hanka <hfernandes238@gmail.com>
2024-03-28 18:14:47 +0100

    Created displayworksData function and export, created generateworks function

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,10 +1,28 @@
 import { worksFetch } from "./config.js";
 
-(async () => {
+export const displayworksData = async () => {
   try {
     const worksData = await worksFetch();
-    console.log(worksData);
+    document.querySelector(".gallery").innerHTML = "";
+    generateworks(worksData);
   } catch (error) {
     console.error(error);
   }
-})();
+};
+
+const generateworks = (elementsArr) => {
+  for (let i = 0, r = elementsArr.length; i < r; i++) {
+    const element = elementsArr[i];
+
+    const divGallery = document.querySelector(".gallery");
+    const figureElement = document.createElement("figure");
+    const imgElement = document.createElement("img");
+    imgElement.src = element.imageUrl;
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.innerText = element.title;
+
+    divGallery.appendChild(figureElement);
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(figcaptionElement);
+  }
+};
