23a36c8ee4b299c21b8460abb6820e46f2c2f0b9
helder-hanka <hfernandes238@gmail.com>
2024-03-31 15:04:03 +0200

    Remove try and updated in loginFetch func

--- FrontEnd/scripts/config.js
+++ FrontEnd/scripts/config.js
@@ -21,14 +21,32 @@ export const loginFetch = async (email, password) => {
     email,
     password,
   };
-  try {
-    const res = await fetch("http://localhost:5678/api/users/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      bady: form,
-    });
-    return res.json();
-  } catch (error) {
-    return error;
-  }
+
+  // try {
+  // return await fetch("http://localhost:5678/api/users/login", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify(form),
+  // });
+  // // messageError(res);
+  // console.log("1");
+  // console.log(messageError(res));
+  // console.log("2");
+
+  // console.log("Res", res);
+  // console.log("Res J", res.json());
+  // messageError(res)
+
+  return await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(form),
+  });
+  // }
 };
+
+// const messageError = (res) => {
+//   if (res.status !== 200) {
+//     throw new Error(`Error: ${res.statusText}, status: ${res.status}`);
+//   }
+// };
